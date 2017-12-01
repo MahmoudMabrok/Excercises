5e6beb1f8211b71f1d0975f60c5d0b523d4fbfe7
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2017-12-01 10:14:28 +0200

    add rate control

--- src/FanSpin.java
+++ src/FanSpin.java
@@ -1,3 +1,4 @@
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -6,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.VBox;
@@ -14,7 +16,6 @@ import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-
 
 public class FanSpin extends Application {
 
@@ -26,14 +27,23 @@ public class FanSpin extends Application {
     public void start(Stage primaryStage) {
         VBox vPane  = new VBox();
         vPane.setPadding(new Insets(20));
-        vPane.setSpacing(25);
+        vPane.setSpacing(18);
         vPane.setAlignment(Pos.CENTER);
 
+
         fanPane f = new fanPane(100 , 100 , 60 ) ;
+
         HBox   hBu = new HBox(25) ;
         Button bPause = new Button("Pause") ;
         Button bResume = new Button("Resume") ;
         Button bReverse = new Button("Reverse") ;
+
+        HBox hrate = new HBox(15) ;
+        hrate.setAlignment(Pos.CENTER);
+        Button bUp = new Button("UP") ;
+        Button bDo = new Button("DOWN") ;
+        hrate.getChildren().addAll(bUp , bDo);
+
 
          EventHandler<ActionEvent> handler = e->{
             f.updateStart();
@@ -41,9 +51,9 @@ public class FanSpin extends Application {
 
         Timeline t = new Timeline(new KeyFrame(javafx.util.Duration.millis(100),handler ))   ;
         t.setCycleCount(Timeline.INDEFINITE);
-        t.setRate(1);
+        t.setRate(40);
         t.play();
-
+        Label lRate = new Label(( ""+(int)t.getRate())) ;
 
         bPause.setOnAction(e->{
             t.pause();
@@ -56,14 +66,21 @@ public class FanSpin extends Application {
             f.r *= -1 ;
             t.play();
         });
+        bUp.setOnAction(e->{
+            if(t.getStatus() != Animation.Status.PAUSED){
+            t.setRate( t.getRate() >= 0 ? t.getRate() + 2 :0 );
+            lRate.setText("" +(int)t.getRate());}
+        });
+        bDo.setOnAction(e->{
+            if(t.getStatus() != Animation.Status.PAUSED) {
+                t.setRate(t.getRate() <= 1 ? 0 : t.getRate() - 2);
+                lRate.setText("" + (int) t.getRate());
+            }
+        });
 
         hBu.getChildren().addAll(bPause , bResume,bReverse) ;
 
-
-
-
-        vPane.getChildren().addAll(f , hBu) ;
-
+        vPane.getChildren().addAll( lRate,f , hBu ,hrate) ;
 
         Scene s =new Scene( vPane) ;
         primaryStage.setResizable(false);
@@ -114,6 +131,8 @@ class fanPane extends Pane {
 
         }
     }
+
+
 
 
    }
